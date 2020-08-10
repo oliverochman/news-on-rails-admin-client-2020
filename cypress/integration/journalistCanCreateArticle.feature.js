6c29b4ce769ac2b784eb7cdc9fda129e7cdb8d56
viktor_sommar <vsommarbaillot@gmail.com>
2020-08-10 13:59:48 +0200

    Writing test for journalist to create article. All failing for now

---
+++ cypress/integration/journalistCanCreateArticle.feature.js
@@ -0,0 +1,33 @@
+describe("Journalist can create article", () => {
+  beforeEach(() => {
+    cy.server()
+    cy.visit("/")
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/auth",
+      response: "fixture:registration_response.json",
+      headers: {
+        uid: "journalist@mail.com"
+      }
+    })
+    cy.get("#login").click()
+    cy.get("#login-form").within(() => {
+      cy.get("#email").type("journalist@mail.com")
+      cy.get("#password").type("password")
+      cy.get("button").contains("Submit").click()
+    })
+  })
+
+  it("journalist can click on create article", () => {
+  cy.get("button").contains("Create Article").click()
+  cy.get("article-form").within(() => {
+    cy.get("#title").type("Title")
+    cy.get("#lead").type("Lead")
+    cy.get("#content").type("This is content")
+    cy.get("button").contains("Save Article").click()
+  })
+
+  it("journalist can see saved article on login")
+  cy.get("article").contains("This is content")
+})
+})
