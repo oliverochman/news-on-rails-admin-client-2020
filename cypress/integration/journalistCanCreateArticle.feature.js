7372b29bb873f29613442f0126088bb13529c75c
felixoppongwillborg <fwlixw@gmail.com>
2020-08-12 15:36:34 +0200

    wrote sadpath for journalist can create article

--- cypress/integration/journalistCanCreateArticle.feature.js
+++ cypress/integration/journalistCanCreateArticle.feature.js
@@ -21,3 +21,26 @@ describe("Journalist can create article", () => {
     });
   });
 });
+
+describe("Journalist can't create article", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.visit("/")
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/articles",
+      response: {"message": "Title can't be blank"},
+    });
+  });
+  it("journalist can create an article successfully", () => {
+    cy.get("button").contains("Create Article").click();
+    cy.get("#article-form").within(() => {
+      cy.get("#lead").type("Lead");
+      cy.get("#category").click()
+      cy.get("div[role='option']").contains("Lifestyle").click()
+      cy.get("#content").type("This is content");
+      cy.file_upload("img.jpeg", "#image-upload", "image/jpeg");
+      cy.get("button").contains("Save Article").click();
+    });
+  });
+})
