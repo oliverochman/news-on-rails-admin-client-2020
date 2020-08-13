a6194365737fc12478047299cece1b69cadb21aa
viktor_sommar <vsommarbaillot@gmail.com>
2020-08-13 15:25:37 +0200

    Adding login features from a previous branch.

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LoginForm = (props) => {
+  return (
+    <form onSubmit={props.authenticate} id="login-form">
+      <lable id="label">Email</lable>
+      <input name="name" type="email" id="email" />
+      <label id="label">Password</label>
+      <input name="password" type="password" id="password" />
+      <button id="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
