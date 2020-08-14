d32b22605b669b5bf3e3aff13c65b8afe252d411
viktor_sommar <vsommarbaillot@gmail.com>
2020-08-14 13:34:47 +0200

    Configuring and setting up redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import axios from 'axios';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import axios from "axios";
+import "./index.css";
+import { Provider } from "react-redux";
+import configureStore from "./state/store/configureStore";
 
-axios.defaults.baseURL = "http://localhost:3000/api/v1"
+axios.defaults.baseURL = "http://localhost:3000/api/v1";
+
+const store = configureStore()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
---
+++ src/state/store/initialState.js
@@ -0,0 +1,5 @@
+const initialState = {
+  message: 'Hello'
+}
+
+export default initialState;
