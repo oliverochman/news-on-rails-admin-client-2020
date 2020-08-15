af30d03cc920b9fc491ef1d80deae5af60bd08cd
viktor_sommar <vsommarbaillot@gmail.com>
2020-08-15 15:26:27 +0200

    Removing commented out code

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,12 @@ import Login from "./components/Login";
 import { connect } from "react-redux";
 
 const App = (props) => {
-  // state = {
-  //   authenticated: false
-  // }
-
-  // let login
-  // this.state.authenticated?(
-  //   login = (
-  //     <p id="login-message">You are currently logged in as {JSON.parse(localStorage.getItem("J-tockAuth-Storage")).uid}</p>
-  //   )
-  // ):(
-  //   login = (
-  //     <Login setAuthenticated={() => this.setState({authenticated: true})} />
-  //   )
-  // )
   return (
     <>
       <Header as="h1" icon textAlign="center">
         <Icon name="train" circular />
         News on Rails
       </Header>
-      {/* <div>
-        {login}
-      </div> */}
       {props.userRole === "journalist" ? <ArticlePage /> : <Login />}
     </>
   );
