4766da00bee6ff3c0a6d3478d67db86f77445447
viktor_sommar <vsommarbaillot@gmail.com>
2020-08-15 11:38:17 +0200

    Rewriting the tests to make them pass with the new code implementation. Now starting with the sad path

--- cypress/integration/journalistCanCreateArticle.feature.js
+++ cypress/integration/journalistCanCreateArticle.feature.js
@@ -7,15 +7,17 @@ describe("Journalist can create article", () => {
         url: "http://localhost:3000/api/v1/articles",
         response: '{"message": "Your article was successfully created"}',
       });
-      cy.visit("/");
       cy.route({
         method: "POST",
         url: "http://localhost:3000/api/v1/auth/sign_in",
         response: "fixture:registration_response.json",
-        headers: {
-          uid: "journalist@mail.com",
-        },
       });
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/auth/**",
+        response: "fixture:registration_response.json",
+      })
+      cy.visit('/')
       cy.get("#login").click();
       cy.get("#login-form").within(() => {
         cy.get("#email").type("journalist@mail.com");
@@ -49,15 +51,17 @@ describe("Journalist can create article", () => {
         url: "http://localhost:3000/api/v1/articles",
         response: { message: "Title can't be blank" },
       });
-      cy.visit("/");
       cy.route({
         method: "POST",
         url: "http://localhost:3000/api/v1/auth/sign_in",
         response: "fixture:registration_response.json",
-        headers: {
-          uid: "journalist@mail.com",
-        },
       });
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/auth/**",
+        response: "fixture:registration_response.json",
+      })
+      cy.visit('/')
       cy.get("#login").click();
       cy.get("#login-form").within(() => {
         cy.get("#email").type("journalist@mail.com");
